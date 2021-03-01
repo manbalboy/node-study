05a39a74048bab808ce64279070c3efc8f596b84
manbalboy <manbalboy@hanmail.net>
2021-03-01 20:22:15 +0900

    모듈테스트

---
+++ 02.module/index.js
@@ -0,0 +1,7 @@
+
+// import  checkStringOddOrEven from'./func.js'
+const checkStringOddOrEven = require('./func.js');
+
+
+console.log(checkStringOddOrEven('홀수입니다'));
+console.log(checkStringOddOrEven('짝수입니다.'));
---
+++ 02.module/testExport.js
@@ -0,0 +1,7 @@
+const test = 1;
+const test1 = 2;
+
+export default {
+    test,
+    test1
+}
